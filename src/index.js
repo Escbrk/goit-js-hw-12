85f4ab81996453d567b0f2b8ce387aef3c25ee6d
Escbrk <69437294+Escbrk@users.noreply.github.com>
2023-12-17 03:20:04 -0700

    upd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import axios from 'axios';
 
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
@@ -8,6 +9,9 @@ const refs = {
   input: document.querySelector('[type="text"]'),
   searchBtn: document.querySelector('[type="submit"]'),
   gallery: document.querySelector('.gallery'),
+  loadBtn: document.querySelector('.js-load'),
+  loader: document.querySelector('.js-loader'),
+  loadDiv: document.querySelector('.load-container'),
 };
 
 const params = {
@@ -15,11 +19,14 @@ const params = {
   BASE_URL: 'https://pixabay.com/api/',
 };
 
+let currentPage = 1;
+// let counter = 0;
+
 let lightbox = new SimpleLightbox('.gallery a');
 
-function getGaleryItems() {
-  return fetch(
-    `${params.BASE_URL}?key=${params.API_KEY}&q=${refs.input.value}&image_type=photo&orientation=horizontal&safesearch=true`
+async function getGaleryItems(page = 1) {
+  const data = await fetch(
+    `${params.BASE_URL}?key=${params.API_KEY}&q=${refs.input.value}&image_type=photo&orientation=horizontal&safesearch=truepage=${page}&per_page=4`
   ).then(resp => {
     if (!resp.ok) {
       throw new Error(resp.statusText);
@@ -27,22 +34,23 @@ function getGaleryItems() {
 
     return resp.json();
   });
+  return data;
 }
 
 refs.searchBtn.addEventListener('click', onSearch);
+refs.loadBtn.addEventListener('click', onClick);
 
 function onSearch(e) {
   e.preventDefault();
-  
+
   if (refs.input.value.trim() === '') {
-    
     return iziToast.error({
       message: 'Sorry, the field must be filled in!',
       position: 'topRight',
     });
   }
 
-  refs.gallery.innerHTML = '<span class="loader"></span>'
+  refs.gallery.innerHTML = '<span class="loader"></span>';
 
   getGaleryItems()
     .then(data => {
@@ -50,8 +58,23 @@ function onSearch(e) {
       lightbox.refresh();
     })
     .catch(err => console.error(err));
+
+  refs.loadBtn.hidden = false;
 }
 
+function onClick(e) {
+  e.preventDefault();
+
+
+  currentPage += 1;
+  getGaleryItems(currentPage).then(data =>
+  {
+    refs.loader.innerHTML = '<span class="loader"></span>'
+    refs.gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits))
+    refs.loader.innerHTML = '';
+    }
+  );
+}
 function createMarkup(arr) {
   if (arr.length === 0) {
     iziToast.error({
